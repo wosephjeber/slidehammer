e453294b8606b4fc6bb812f9dc6321a1d8cb7402
Joe Weber <wosephjeber@gmail.com>
2015-09-16 21:08:40 -0500

    Get started with grunt

---
+++ gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/* <%= pkg.name %> v<%= pkg.version %> \n' +
+      ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> \n */\n',
+    
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/slidehammer.css' : 'sass/slidehammer.scss'
+        }
+      },
+      production: {
+        options: {
+          style: 'compact',
+          sourcemap: 'none',
+          banner: '<%= banner %>'
+        },
+        files: {
+          'dist/slidehammer.min.css' : 'sass/slidehammer.scss'
+        }
+      }
+    },
+    
+    uglify: {
+      production: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        files: {
+          'dist/slidehammer.min.js': ['js/slidehammer.js']
+        }
+      }
+    },
+    
+    watch: {
+      default: {
+        files: ['sass/**/*.scss', 'js/**/*.js'],
+        tasks: ['default']
+      },
+      options: {
+        interrupt: false,
+        nospawn: true,
+        event: 'all',
+        interval: 1000,
+        debounceDelay: 1000
+      },
+    }
+    
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', [ 'sass:dev', 'sass:production', 'uglify:production' ] );
+
+};
